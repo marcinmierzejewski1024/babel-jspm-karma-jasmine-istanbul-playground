67d206913a2b681c5d3bc8cde6e64b2cf59d9f48
Marcin Mierzejewski <m.mierzejewski@softbay.pl>
2016-05-04 16:15:11 +0200

    Initial commit

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+/* global module */
+module.exports = function (config) {
+    'use strict';
+    config.set({
+        autoWatch: true,
+        singleRun: true,
+
+        frameworks: ['jspm', 'jasmine'],
+
+        jspm: {
+            config: 'src/config.js',
+            loadFiles: [
+                'src/*.spec.js'
+            ],
+            serveFiles: [
+                'src/!(*spec).js'
+            ]
+        },
+
+        proxies: {
+            '/src/': '/base/src/',
+            '/jspm_packages/': '/src/jspm_packages/'
+        },
+
+        browsers: ['PhantomJS'],
+
+        reporters: ['progress'],
+        files: [
+            'node_modules/babel-polyfill/dist/polyfill.js'
+        ]
+    });
+
+};
